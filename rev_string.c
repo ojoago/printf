94923cde1688cb0b2a7b42c45efd2914f732373f
zamanisonkay@gmail.com <zamanisonkay@gmail.com>
2021-07-27 17:55:14 -0700

    rev string

---
+++ rev_string.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+ /**
+ * format_r - A function reverses string passed in.
+ * @valist: argument passed in
+ * @buffer: values stored
+ * @index: returns pointer to index
+ */
+void format_r(va_list valist, char *buffer, int *index)
+{
+int i, strlen;
+char *s;
+s = va_arg(valist, char *);
+strlen = _strlen(s) - 1;
+for (i = strlen; i >= 0; i--, *index += 1)
+{
+buffer[*index] = s[i];
+}
+}
